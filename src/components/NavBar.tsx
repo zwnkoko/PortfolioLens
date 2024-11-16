d17e3af9d913166c4a543911975d385e46301de9
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-11-17 02:33:31 +0800

    set up home

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <nav className="text-white">something navbar</nav>;
+};
+
+export default NavBar;
---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const EXTERNAL_LINKS = {
+  github: "https://github.com/zwnkoko/PortfolioLens",
+};
