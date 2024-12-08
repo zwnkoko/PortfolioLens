f6c3d06cc1b2ee8c669b71afd5e0276be1283e2d
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-12-08 22:16:35 +0000

    restyle home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,22 @@
-import SearchField from "../components/SearchField";
-import Button from "../components/Button";
 import NavBar from "../components/NavBar";
-import { EXTERNAL_LINKS } from "../config";
+import Button from "../components/Button";
 
 const Home = () => {
-  const handleGhubClick = () => {
-    window.open(EXTERNAL_LINKS.github, "_blank"); // Opens the GitHub link in a new tab
-  };
+
   return (
-    <div className="bg-blue-100 bg-gradient-to-b from-[#0a0f24] to-[#1c1c1e] min-h-dvh flex flex-col">
+    <div className="lg:max-w-7xl mx-auto px-6 min-h-dvh flex flex-col">
+
       <header>
         <NavBar />
       </header>
+
       <main className="flex flex-col justify-center items-center flex-grow">
-        <h1 className="text-white font-bold text-3xl lg:text-6xl">
-          Portfolio Lens
-        </h1>
-        <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 text-xs lg:text-xl">
-          Simple & Minimalistic portfolio tracker app for retail investors
+        <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 text-center text-2xl md:text-6xl">
+          Minimalistic Portfolio Tracker for Retail Investors
         </p>
-        <Button
-          value="Source Codes"
-          prefix="src/assets/github-mark-white.svg"
-          onClick={handleGhubClick}
-          variant="tertiary"
-        />
+        <Button value="Get Started"/>
       </main>
+
     </div>
   );
 };
