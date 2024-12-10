5b0079feee54af8e32bd91a5dc08e3fd6efec1e2
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-12-10 21:08:38 +0000

    logic for fetching data

---
+++ src/api/homeTickers.ts
@@ -0,0 +1,20 @@
+import { historicalDataEP } from "../endpoints";
+
+export const fetchHomeTickers = async (ticker : string) => {
+    const prices: {x: string, y: number}[] = [];
+    const endPoint = historicalDataEP(ticker);
+    console.log(endPoint)
+
+    try{
+      const response = await fetch(endPoint);
+      const data = await response.json();
+      const fiveDaysPrices = data.historical.slice(0,5).reverse();
+      for(let price of fiveDaysPrices){
+        prices.push({ x: price.date, y: price.adjClose });
+      }
+      return prices;
+    } catch(error) {
+        console.error("Error fetching home tickers data", error);
+        throw(error);
+    }
+}
